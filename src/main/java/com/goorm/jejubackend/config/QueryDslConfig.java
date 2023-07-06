2582fae0d60a659ac22ee62b5cb2ba428745cac9
changmoo <public.ellerymoon@gmail.com>
2023-07-06 17:02:29 +0900

    init config - 3

--- src/main/java/com/goorm/jejubackend/config/QueryDslConfig.java
+++ src/main/java/com/goorm/jejubackend/config/QueryDslConfig.java
@@ -1,2 +1,19 @@
-package com.goorm.jejubackend.config;public class QueryDslConfig {
+package com.goorm.jejubackend.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
 }
---
+++ src/main/java/com/goorm/jejubackend/domain/base/BaseEntity.java
@@ -0,0 +1,22 @@
+package com.goorm.jejubackend.domain.base;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedBy
+    protected String createdBy;
+
+    @CreatedDate
+    protected LocalDateTime createdAt;
+
+}
