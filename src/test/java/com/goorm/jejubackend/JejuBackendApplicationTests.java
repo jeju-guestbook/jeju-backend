038215459434b19cedc4f02248a069f1467165a4
changmoo <public.ellerymoon@gmail.com>
2023-07-06 09:57:44 +0900

    Initial commit

---
+++ src/test/java/com/goorm/jejubackend/JejuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goorm.jejubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JejuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
