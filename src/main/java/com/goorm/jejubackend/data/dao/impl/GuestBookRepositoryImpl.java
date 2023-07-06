b8c9fd3241055bfab0b1b384de090b4a84cc0791
changmoo <public.ellerymoon@gmail.com>
2023-07-06 20:44:54 +0900

    리포지토리 구현체 추가

---
+++ src/main/java/com/goorm/jejubackend/data/dao/impl/GuestBookRepositoryImpl.java
@@ -0,0 +1,17 @@
+package com.goorm.jejubackend.data.dao.impl;
+
+import com.goorm.jejubackend.data.dao.GuestBookRepository;
+import com.goorm.jejubackend.data.entity.GuestBook;
+
+public class GuestBookRepositoryImpl implements GuestBookRepository {
+
+    @Override
+    public GuestBook insertGuestBook(final GuestBook guestBook) {
+        return null;
+    }
+
+    @Override
+    public GuestBook selectGuestBookList(final Integer page) {
+        return null;
+    }
+}
