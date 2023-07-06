e136c4fc6a74325ed779b066a41bca5cfaf0c6af
changmoo <public.ellerymoon@gmail.com>
2023-07-06 20:44:46 +0900

    리포지토리 추가 - 2

--- src/main/java/com/goorm/jejubackend/data/dao/GeneratedPostRepository.java
+++ src/main/java/com/goorm/jejubackend/data/dao/GeneratedPostRepository.java
@@ -1,5 +1,11 @@
 package com.goorm.jejubackend.data.dao;
 
+import com.goorm.jejubackend.data.entity.GeneratedPost;
+import java.util.List;
+
 public interface GeneratedPostRepository {
 
+    GeneratedPost insertGeneratedPost(GeneratedPost generatedPost);
+
+    List<GeneratedPost> selectMyGeneratedPost(Long id);
 }
--- src/main/java/com/goorm/jejubackend/data/dao/GuestBookRepository.java
+++ src/main/java/com/goorm/jejubackend/data/dao/GuestBookRepository.java
@@ -1,5 +1,11 @@
 package com.goorm.jejubackend.data.dao;
 
+import com.goorm.jejubackend.data.entity.GuestBook;
+
 public interface GuestBookRepository {
-    
+
+    GuestBook insertGuestBook(GuestBook guestBook);
+
+    GuestBook selectGuestBookList(Integer page);
+
 }
