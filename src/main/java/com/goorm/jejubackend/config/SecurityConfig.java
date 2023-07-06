ba5b911fcda3fa17ea168d871359b5fc35cb7694
changmoo <public.ellerymoon@gmail.com>
2023-07-07 01:45:52 +0900

    개발 환경 분리

---
+++ src/main/java/com/goorm/jejubackend/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.goorm.jejubackend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // h2 database를 사용하기 위해 메소드 오버라이딩
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+        throws Exception{
+        auth.inMemoryAuthentication()
+            .withUser("root")
+            .password("five1945")
+            .roles("USER"); // 유저권한부여
+    }
+}
